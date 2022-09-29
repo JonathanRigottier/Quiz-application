038e1a0c3cf17847e7856658c8cf1f3fc5cee09c
Jonathan Rigottier <jrigottier@gmail.com>
2022-09-29 21:58:44 +0300

    Categories added on the quizz application

--- src/main/java/pl/sdacademy/projektplus/quiz/frontend/FrontendController.java
+++ src/main/java/pl/sdacademy/projektplus/quiz/frontend/FrontendController.java
@@ -1,11 +1,20 @@
 package pl.sdacademy.projektplus.quiz.frontend;
 
+import lombok.extern.java.Log;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import pl.sdacademy.projektplus.quiz.services.QuizDataService;
 
 @Controller
+@Log
 public class FrontendController {
+
+    @Autowired
+    private QuizDataService quizDataService;
 
     @GetMapping("/")
     public String hello(Model model) {
@@ -15,6 +24,13 @@ public class FrontendController {
     @GetMapping("/select")
     public String select(Model model) {
         model.addAttribute("gameOptions", new GameOptions());
+        model.addAttribute("categories", quizDataService.getQuizCategories());
         return "select";
+    }
+
+    @PostMapping("/select")
+    public String postSelectForm(Model model, @ModelAttribute GameOptions gameOptions) {
+        log.info("Form submitted with data: " + gameOptions);
+        return "index";
     }
 }
