bf12192d9576c1cef6c0c4e3f569aef9b2cae90b
Jonathan Rigottier <jrigottier@gmail.com>
2022-09-29 09:49:51 +0300

    Creation of a player in the database implemented

--- src/main/java/pl/sdacademy/projektplus/quiz/rest/HealthcheckRestController.java
+++ src/main/java/pl/sdacademy/projektplus/quiz/rest/HealthcheckRestController.java
@@ -3,13 +3,15 @@ package pl.sdacademy.projektplus.quiz.rest;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import pl.sdacademy.projektplus.quiz.dto.HealthcheckDto;
 
 @RestController
 @RequestMapping("/api/health")
 public class HealthcheckRestController {
 
     @GetMapping
-    public String healthcheck() {
-        return "It's working!";
+    public HealthcheckDto healthcheck() {
+        HealthcheckDto dto = new HealthcheckDto(true, "It's working!");
+        return dto;
     }
 }
